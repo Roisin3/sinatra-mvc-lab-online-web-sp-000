6b0ee08e3f117a71da53de5a8279178e06ee2b8d
Michael Lynch <michael@michaelrlynch.com>
2020-03-04 03:47:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,27 @@
 class PigLatinizer
   attr_reader :text
 
-def piglatinizer(input_str)
-  input_str.split(" ").length == 1 ? pl_word(input_str) : pl_phrase(input_str)
-end
-
-def consonant?(letter)
-  !letter.match(/[aAeEiIoOuU]/)
-end
-
-def pl_word(word)
-  if !consonant?(word[0])
-    word = word + 'w'
-  elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-    word = word[3..-1] + word[0..2]
-  elsif consonant?(word[0])
-    word = word[1..-1] + word[0]
+  def piglatinizer(input_str)
+    input_str.split(" ").length == 1 ? pl_word(input_str) : pl_phrase(input_str)
   end
-  word + 'ay'
-end
 
-def pl_phrase(phrase)
-  phrase.split.collect {|word| pl_word(word)}.join(" ")
-end
+  def consonant?(letter)
+    !letter.match(/[aAeEiIoOuU]/)
+  end
+
+  def pl_word(word)
+    if !consonant?(word[0])
+      word = word + 'w'
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word[3..-1] + word[0..2]
+    elsif consonant?(word[0])
+      word = word[1..-1] + word[0]
+    end
+    word + 'ay'
+  end
+
+  def pl_phrase(phrase)
+    phrase.split.collect {|word| pl_word(word)}.join(" ")
+  end
 
 end
